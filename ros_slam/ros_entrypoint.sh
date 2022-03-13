b3af2ecb5cfe0d216fc17e834babd2676806b780
NekoSaiKou <ricky0711.yang@gmail.com>
2022-03-13 17:01:31 +0800

    Feat: Add realsense ros to bashrc

--- ros_slam/ros_entrypoint.sh
+++ ros_slam/ros_entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-if [ ! -f "~/.bashrc" ] 
-then
-    cp /etc/skel/.bashrc ~/
-    echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
-fi
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
 # setup ros environment
 source /opt/ros/${ROS_DISTRO}/setup.bash
 exec "$@"
