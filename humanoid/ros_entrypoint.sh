3b4654b42caac849aefb53e276b772f5b8d707bd
NekoSaiKou <ricky0711.yang@gmail.com>
2022-03-20 16:02:11 +0800

    Fix: Fix Humanoid Qt not found error

--- humanoid/ros_entrypoint.sh
+++ humanoid/ros_entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/home/${USERNAME}/qt5/Qt5.12/lib" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/${USERNAME}/qt5/Qt5.12/lib" >> ~/.bashrc
 echo "export Qt5_DIR=/home/${USERNAME}/qt5/Qt5.12/lib/cmake/Qt5" >> ~/.bashrc
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 
 # setup ros environment
 source /opt/ros/${ROS_DISTRO}/setup.bash
