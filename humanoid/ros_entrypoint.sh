4b987a41eaf8b501e52c8ccbf29f4e9e9476cd7d
NekoSaiKou <ricky0711.yang@gmail.com>
2022-03-27 14:52:54 +0800

    Fix: Append LDLibrary before source ros setup

--- humanoid/ros_entrypoint.sh
+++ humanoid/ros_entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 if [ ! -f "~/.bashrc" ] 
 then
     cp /etc/skel/.bashrc ~/
-    echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
     echo "export LD_LIBRARY_PATH=/home/${USERNAME}/qt5/Qt5.12/lib:${LD_LIBRARY_PATH}" >> ~/.bashrc
     echo "export Qt5_DIR=/home/${USERNAME}/qt5/Qt5.12/lib/cmake/Qt5" >> ~/.bashrc
+    echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 fi
 
 # setup ros environment
