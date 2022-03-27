27c9385d47b0d4150e7b2582656cf02563cfd9de
NekoSaiKou <ricky0711.yang@gmail.com>
2022-03-27 14:25:37 +0800

    Fix: Fix LD Path order for standalone Qt install

--- humanoid/ros_entrypoint.sh
+++ humanoid/ros_entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/${USERNAME}/qt5/Qt5.12/lib" >> ~/.bashrc
-echo "export Qt5_DIR=/home/${USERNAME}/qt5/Qt5.12/lib/cmake/Qt5" >> ~/.bashrc
-echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+if [ ! -f "~/.bashrc" ] 
+then
+    cp /etc/skel/.bashrc ~/
+    echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+    echo "export LD_LIBRARY_PATH=/home/${USERNAME}/qt5/Qt5.12/lib:${LD_LIBRARY_PATH}" >> ~/.bashrc
+    echo "export Qt5_DIR=/home/${USERNAME}/qt5/Qt5.12/lib/cmake/Qt5" >> ~/.bashrc
+fi
 
 # setup ros environment
 source /opt/ros/${ROS_DISTRO}/setup.bash
